b2bd756cc61d4333f3028df3adb2da7d5ad20f92
Anoob Bava <anoob.bava@gmail.com>
2018-11-06 16:02:25 +0530

    quotes added with new quote component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
 <div class="container">
-  
+  <app-new-quote @quoteAdded='updateQuote'></app-new-quote>
+  <app-quote-grid :quotes="quoteData"></app-quote-grid>
 </div>
   
 </template>
 
 <script>
-export default {
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue'
 
+export default {
+  data(){
+    return{
+      maxQuote: 10,
+      quoteData: ['this is my test quote']
+    }
+  },
+  methods:{
+    updateQuote(data){
+      this.quoteData.push(data);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+
+  }
 }
 </script>
 
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm8-col-sm-offset-2 form-group">
+                <label>new Quote</label>
+                <textarea class="form-control" cols="10" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm8-col-sm-offset-2 form-group">
+                <button class="btn btn-primary" @click.prevent="addQuote">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                quote: ''
+            }
+        },
+        methods:{
+            addQuote(){
+                this.$emit('quoteAdded',  this.quote);
+                this.quote = '';
+            }
+        },
+        components: {
+
+        }
+    }
+    
+</script>
+
+<style>
+    
+</style>
